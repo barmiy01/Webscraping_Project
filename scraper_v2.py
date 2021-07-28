dc0e415e22bcec645ce1023a2272e1f1fc8c4629
Bilal <barmiyawo@st.ug.edu.gh>
2021-07-28 18:17:11 +0100

    boiling_point_added_to_columns

--- scraper_v2.py
+++ scraper_v2.py
@@ -15,24 +15,19 @@ element_features = {'Element_Name':[], 'Atomic_Number':[], 'Electronegativity':[
 
 element_list = driver.find_elements_by_xpath("//li[@class='p-md-bottom print-avoid-break-inside print-padding-top']")
 
-
-# %%
-#%%
-#print(len(element_list))
-
 for item in element_list[0:103]:
     element_name = item.find_element_by_xpath(".//div[@class='f-125 f-lh-15']")
     atomic_number = item.find_element_by_xpath(".//div[@class='f-1 f-lh-15']")
     electronegativity = item.find_elements_by_xpath(".//span[@class='capitalized']")[3]
-    #boiling_point =  item.find_element_by_xpath('')
+    boiling_point =  item.find_element_by_xpath('')
     element_features['Element_Name'].append(element_name.text)
     element_features['Atomic_Number'].append(atomic_number.text)
-    if electronegativity.text is float:
-        element_features['Electronegativity'].append(electronegativity.text)
-    else:
-        pass
+    element_features['Electronegativity'].append(electronegativity.text)
+    element_features['Boiling_Point'].append(boiling_point.text)
 
 # %%
 #%%
-print(element_features)
+element_data = pd.DataFrame(element_features)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(element_data)
 # %%
