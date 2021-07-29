57ee8a8fe98ee64fc5db348fcbde9779dff7cfc4
Bilal <barmiyawo@st.ug.edu.gh>
2021-07-29 16:20:26 +0100

    pending_volume_mistake_correction

--- OQMD_Database_scraper.py
+++ OQMD_Database_scraper.py
@@ -7,16 +7,40 @@ from selenium.webdriver.support.ui import Select
 import pandas as pd
 import time
 
-URL = f"https://pubchem.ncbi.nlm.nih.gov/periodic-table/#view=list"
+URL = f"http://oqmd.org/api/search#apisearchresult"
 driver = webdriver.Chrome('chromedriver')
 driver.get(URL)
 
 element_features = {'Name':[], 'Spacegroup':[], 'Volume':[], 'Band_gap':[]}
 
-element_list = driver.find_elements_by_xpath("//*[@class='clickableRow']")
+search_button = driver.find_element_by_xpath('//*[@id="submit-id-search"]')
+search_button.click()
 
+table = driver.find_element_by_xpath('//*[@id="ResultTable"]/tbody')
+element_list = table.find_elements_by_xpath('.//tr')[1:]
+for _ in range(20):
+    next_page_button = driver.find_element_by_xpath('//button[@class="next"]')
+    #redefined due to the staleElement error
+    table = driver.find_element_by_xpath('//*[@id="ResultTable"]/tbody')
+    element_list = table.find_elements_by_xpath('.//tr')[1:]
+    
+    for item in element_list:
+        name = item.find_element_by_xpath('.//td[@width="60"]')
+        spacegroup = item.find_elements_by_xpath('.//td[@width="50"]')[3]
+        volume = item.find_element_by_xpath('.//td[@width="80"]')
+        band_gap = item.find_elements_by_xpath('.//td[@width="50"]')[5]
+        element_features['Name'].append(name.text)
+        element_features['Spacegroup'].append(spacegroup.text)
+        element_features['Volume'].append(volume.text)
+        element_features['Band_gap'].append(band_gap.text)
+        time.sleep(1)
+    
+    next_page_button.click()
+    time.sleep(10)
 
 # %%
 #%%
-print(element_list)
+element_data = pd.DataFrame(element_features)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(element_data)
 # %%
